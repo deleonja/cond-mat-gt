41ff0348a5daed597e93e0972ca2e34f965c0f91
deleonja <deleongarrido.jose@gmail.com>
2021-01-15 11:39:21 -0600

    matrix_form added

--- python/quantumCh.py
+++ python/quantumCh.py
@@ -229,6 +229,46 @@ def positivity_test(A):
     positivity = smallest_eig >= 0
     return positivity
 
+def matrix_form(matrix, limiter='p'):
+    """
+    Pretty print of a matrix with different delimiters depending on 'p', 'b'or 'v'
+    """
+    if limiter == 'p':
+        ls, li = '⎛', '⎝'
+        rs, ri = ' ⎞', ' ⎠'
+    elif limiter == 'b':
+        ls, li = '⎡', '⎣'
+        rs, ri = ' ⎤', ' ⎦'
+    elif limiter == 'v':
+        ls, li = '⎢', '⎢'
+        rs, ri = ' ⎥', ' ⎥'
+    else:
+        ls, li = '⎡', '⎣'
+        rs, ri = ' ⎤', ' ⎦'
+    max = 0
+    for row in matrix:
+        for ele in row:
+            if len(str(ele)) > max:
+                max = len(str(ele))
+    print_str = ''
+    for i in range(len(matrix)):
+        if i == 0:
+            print_str += f'{ls}'
+        elif i==(len(matrix)-1):
+            print_str += f'{li}'
+        else:
+            print_str += '⎢'
+        for j in range(len(matrix[i])):
+            print_str += ' {val:^{max}}'.format(val=matrix[i][j], max=max)
+        if i == 0:
+            print_str += f'{rs}\n'
+        elif i==(len(matrix)-1):
+            print_str += f'{ri}'
+        else:
+            print_str += ' ⎥\n'
+    print(print_str, end='\n\n')
+    return
+
 def HSInnerP(A, B):
 	'''
     Computes the Hilbert-Schimidt inner product.
