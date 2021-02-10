4ee2b2609c4391c483b769c6b4321a083fe95069
deleonja <deleongarrido.jose@gmail.com>
2021-02-09 20:06:41 -0600

    Se fusionó lo útil en quantumCh.py

--- python/quantumCh.py
+++ python/quantumCh.py
@@ -1,11 +1,133 @@
-import numpy as np
-from itertools import product, combinations
-import sys
+###############################################################################
+###############################################################################
+#                                quantumCh.py
+#
+#       quantumCh.py is a Python module that contains quantum mechanics
+#       and quantum channels related functions.
+#
+#       Collaborators:
+#           1. Jose Alfredo de León, deleongarrido.jose@gmail.com
+#           2. Alejandro Barillas
+#           3. Amado Cabrera
+#
+#       Feel free to contact any of the collaboratos in case you have any question.
+#
+#
+#       Last updated: 09.02.2021
+###############################################################################
+###############################################################################
 
-# Felipe
+import numpy as np
+from itertools import product
+from math import log
+
+###############################################################################
+#                    QUANTUM MECHANICS-RELATED FUNCTIONS
+#
+#      1. Dirac(vector, nomenclature): prints vector in Dirac notation using
+#         computational (10) or plus-minus (+-) nomenclature.
+#      2. CPTest: CPTest(E) tests the complete positivity of a matrix E.
+###############################################################################
+
+def Comm(A, B):
+    '''
+    Comm(A, B) computes the commutator of A and B.
+
+    ### __Parameters:__
+    A, B: 2D array_like
+        Operators acting on a Hilbert space.
+
+    ### __Returns:__
+    out: 2D array_like
+        AB - BA
+
+    _Author: J.A. de León_
+    '''
+    return np.matmul(A, B) - np.matmul(B, A)
+
+def Dirac(vector, nomenclature='10'):
+    '''
+    Dirac(vector, nomenclature) prints _vector_ in Dirac notation using
+    computational (10) or plus-minus (+-) nomenclature for state vectors
+    of a system of spin-1/2 particles.
+
+    ### __Parameters:__
+    vector: 1D array_like
+        State vector of a system of spin-1/2 particles.
+
+    nomenclature (kwarg): default '10' or '+-'
+        Nomenclature used to print kets.
+
+    ### __Returns:__
+    out: string
+        Vector in Dirac notation.
+
+    ### __Example:__
+    Dirac(np.array([1,-4,2,-3])) = |00> - 4|01> + 2|10> - 3|11>.
+
+    Dirac(np.array([1,-4,2,-3]), nomenclature='+-') = |++> - 4|+-> + 2|-+> - 3|-->
+
+    _Author: J.A. de León_
+    '''
+    particles = int(log(len(vector), 2))
+    strings = [];
+    for i in np.argwhere(vector).flatten():
+        if len(np.binary_repr(i)) < particles:
+          strings.append((particles-len(np.binary_repr(i)))*"0" + str(np.binary_repr(i)))
+        else:
+          strings.append(str(np.binary_repr(i)))
+
+    PlusMinusNotation = []
+
+    for string in strings:
+        dummy = string.replace("0","+")
+        PlusMinusNotation.append(dummy.replace("1", "-"))
+
+    if nomenclature == '+-':
+        stringsToUse = PlusMinusNotation
+    else:
+        stringsToUse = strings
+
+    k = 0
+    for i in range(len(strings)):
+        if k != 0:
+          if vector[int(strings[i], 2)] > 0:
+            print(" + ", end="")
+          elif vector[int(strings[i], 2)] < 0:
+            print(" - ", end="")
+        if vector[int(strings[i], 2)] == 1:
+          coef = ""
+        else:
+          coef = str(vector[int(strings[i], 2)])
+        if str(vector[int(strings[i], 2)])[0] == "-":
+          coef = str(vector[int(strings[i], 2)])[1:]
+          if vector[int(strings[i], 2)] == -1:
+            coef = ""
+        print(coef + "|" + stringsToUse[i] + ">", end="")
+        if strings.index(strings[i]) == len(strings)-1:
+          break
+        k += 1
+    return
+
 def Pauli(indices):
     '''
-    Recursive function to calculate the tensor product of Pauli matrices.
+    Pauli(indices) computes the tensor product of Pauli matrices with indices
+    in _indices_.
+
+    ### __Parameters:__
+    indices: 1D array_like
+        Indices of Pauli matrices.
+
+    ### __Returns:__
+    out: 2D array_like
+        Tensor product of Pauli matrices.
+
+    ### __Examples:__
+    Pauli([1,2]) = sigma_x $tensor$ sigma_y
+
+    Pauli([2,3,0]) = sigma_y $tensor$ sigma_z $tensor$ identity
+
+    _Author: J.A. de León_
     '''
     # Pauli matrices:
     s1 = np.array([[0,1],[1,0]])        # sigma_x
@@ -20,197 +142,18 @@ def Pauli(indices):
     else:
         return np.kron(sigma[indices.pop(0)], Pauli(indices))
 
-def S(N,alpha,n):
-	"""
-	PAULI OPERATORS FUNCTION
-	N: the number of qubits
-	alpha: the Pauli matrix: 1->x, 2->y, 3->z
-	n: label of the qubit
-	"""
-
-	# Pauli basis:
-	sx = np.array([[0,1],[1,0]])
-	sy = np.array([[0,-1j],[1j,0]])
-	sz = np.array([[1,0],[0,-1]])
-	sid = np.array([[1,0],[0,1]])
-	s0 = np.array([[0,0],[0,0]])
-
-	if alpha == 1: #if we define the x spin operator
-		if n == 0: #if we consider the first site of the chain
-			Sn = np.kron(sx,np.identity(2**(N-1)))
-		else: #for the rest of the chain
-			Sn = np.kron(np.kron(np.identity(2**(n-1)),sx),np.identity(2**(N-n)))
-	elif alpha == 2: #if we define the y spin operator
-		if n == 0: #if we consider the first site of the chain
-			Sn = np.kron(sy,np.identity(2**(N-1)))
-		else: #for the rest of the chain
-			Sn = np.kron(np.kron(np.identity(2**(n-1)),sy),np.identity(2**(N-n)))
-	elif alpha == 3: #if we define the z spin operator
-		if n == 0: #if we consider the first site of the chain
-			Sn = np.kron(sz,np.identity(2**(N-1)))
-		else: #for the rest of the chain
-			Sn = np.kron(np.kron(np.identity(2**(n-1)),sz),np.identity(2**(N-n)))
-	elif alpha == 0:
-		Sn = np.identity(2**N)
-	return Sn
-# aquí termina Felipe
-
-# Alejandro
-def change_of_basis_matrix_P_to_C(n):
-	alpha = [0,1,2,3]
-	R = np.array(list(product(alpha, repeat=(n))))
-
-	# Create a 2D square array to storage in each row every vector of the Pauli basis
-	change_of_basis_M = np.zeros((4**n, 4**n))
-
-	# Actually asign to each row of change_of_basis_M every vector of the Pauli basis
-	for i in range(4**n):
-		dummy = np.identity(2**n)
-		for j in range(n):
-			dummy = dummy @ S(n,R[i][j],j+1)
-		change_of_basis_M[i] = dummy.reshape(4**n)
-
-	# Now that you have each column in every row of the change-of-basis matrix in change_of_basis_M transpose it to get the actual
-	# change-of-basis matrix
-	# M_cb_PaC = np.column_stack(change_of_basis_M)
-	change_of_basis_M = change_of_basis_M.T
-
-	return change_of_basis_M
-# aquí termina Alejandro
-
-def combinations_of_components(n, num_of_corr):
-	rIndices = list(range(4**n-1))
-
-	positions = list(combinations(rIndices, num_of_corr))
-
-	r = np.zeros((len(positions),4**n))
-
-	for i in range(len(positions)):
-		r[i][0] = 1
-		for j in positions[i]:
-			r[i][j+1] = 1
-	return r
-
-# Esta función se puede y debe sustituir por np.diag()
-def diagonalMatrix(dim, diagEntries):
-	# Constructs a diagonal square matrix.
-
-	# dim : dimension of the matrix, int expected
-	# diagEntries : list with diagonal entries, list expected
-
-	if (type(dim) != int):
-		sys.exit("Integer expected as first argument of diagonalMatrix function.")
-
-	if (type(diagEntries) != list):
-		sys.exit("List expected as second argument of diagonalMatrix function.")
-
-	if (len(diagEntries) != dim):
-		sys.exit("In diagonalMatrix function: length of diagonal-entries' list does not match dimension of the matrix.")
-
-	A = np.zeros((dim, dim))
-
-	for i in range(dim):
-		A[i][i] = diagEntries[i]
-
-	return A
-
-def onesAndZeros(oneIndices, listLength):
-	# Construct a list of 1's and 0's from tuple of indices of 1's.
-
-	#if ( (type(oneIndices) != tuple) or (type(oneIndices) != list)):
-	#	sys.exit("List or a tuple expected as argument of onesAndZeros.")
-
-	List = [0]*(listLength)
-
-	for i in oneIndices:
-		List[i] = 1
-
-	return List
-
-def maps_in_Pauli_basis(n, param, num_of_corr):
+def PTest(A):
     '''
-    Constructs, in the Pauli basis, all the posible maps that leave invariant a certain qbit-system's number of components.
+    PTest(A) returns True if A is a positive-semidefinite matrix.
 
-    Input:
-    - n: number of qbits
-    - param: number of maps to consider (usually depends on the number of components to leave invariant)
-    - num_of_corr: number of components to leave invariant
+    ### __Parameters:__
+    A: 2D square array_like
+        Matrix.
+    ### __Returns:__
+    out: boolean
+        True if positive-semidefinite holds, false if not.
 
-    Returns an array with param number of all possible maps that leave invariant the num_of_corr n-qbit-system's number of components.
-    '''
-    # Storage all possible combinations of arrays of size 4**n with num_of_corr ones and the rest zeroes
-    r = combinations_of_components(n, num_of_corr)
-
-    # Create an array to storage the param number of maps (square matrices of size 4**n)
-    maps_P = np.zeros((param, 4**n, 4**n))
-
-    # Assign to every diagonal element of every map its correspondent element in each array of posible combinations of 1's and 0's
-    for i in range(param):
-        for j in range(4**n):
-            maps_P[i][j][j] = r[i][j]
-
-    # Return all possible maps in the Pauli basis
-    return maps_P
-
-def change_of_basis(A, change_of_basis_M):
-	'''
-	Makes a change of a basis of a matrix.
-
-	Input:
-	- A: matrix (2D square np.array)
-	- change_of_basis_M: change-of-basis matrix (2D square np.array)
-
-	Returns the matrix in the new basis.
-	'''
-
-	# Calculate de inverse of the change-of-basis matrix
-	inverse_change_of_basis_M = np.linalg.inv(change_of_basis_M)
-
-	# Compute de change of basis
-	matrix_in_new_basis = np.matmul(change_of_basis_M, np.matmul(A, inverse_change_of_basis_M))
-
-	return matrix_in_new_basis
-
-def reshuffle(A, n):
-	'''
-	Computes the reshuffling of a square matrix of size 4**n.
-
-	Input:
-	- A: 2D squared np.array
-	- n: number of qbits in the system
-
-	Returns the dynamical matrix (also known as Choi matrix) of A.
-	'''
-	# Calculate the dimension of the density matrix of the qbit system
-	rho_size = 2**n
-
-	# Create a 2D square array to storage the dynamical matrix
-	dynamical_Matrix = np.zeros((4**n,4**n))
-
-	# Compute the reshuffling of A
-	k = 0 # qbit number label (?)
-	p = 0 # index of the rows after every row in A has been reshaped in a square matrix of size 2**n
-	for i in range(4**n): # Go over each of the 4**n rows of D
-		if i % rho_size == 0 and i != 0: # If it has completed a rho_size number of rows of A
-			k = k + 1
-			p = 0
-		for j in range(rho_size): # Each iteration goes over 4**n elements in a row of D
-			dynamical_Matrix[i][rho_size*j:rho_size*(j+1)] = A[rho_size*k+j].reshape((rho_size,rho_size))[p]
-		p = p + 1
-
-	return dynamical_Matrix
-
-def chop(expr, *, max=1e-10):
-	return [i if abs(i) > max else 0 for i in expr]
-
-def positivity_test(A):
-    '''
-    Checks the positivity of a matrix.
-
-    Input:
-    - A: matrix
-
-    Returns a boolean value. True if A is indeed positive and false if it's not.
+    _Author: J.A. de León_
     '''
     # Calculate the smallest eigenvalue of A
     smallest_eig = chop(np.linalg.eigh(A)[0])[0]
@@ -219,9 +162,147 @@ def positivity_test(A):
     positivity = smallest_eig >= 0
     return positivity
 
-def matrix_form(matrix, limiter='p'):
+def HSInnerP(A, B):
+	'''
+    HSInnerP(A, B) computes the Hilbert-Schmidt inner product of A and B.
+
+    ### __Parameters:__
+    A, B: 2D array_like
+        Operators acting on Hilbert-Schmidt space.
+
+    ### __Returns:__
+    out: float
+        HS inner product between A and B.
+
+    _Author: A. Barillas_
+    '''
+
+    # Definition of HS inner product
+	output = np.trace(np.matmul(np.transpose(np.conjugate(A)),B))
+	return output
+
+def Reshuffle(E):
+    '''
+    Reshuffle(A) reshuffles A, with A a superoperator acting on density matrices
+    of a system of spin 1/2 particles.
+
+    ### __Parameters__:
+    A: 2D array_like
+        Superoperator acting on density matrices of a system of spin 1/2 particles.
+
+    ### __Returns__:
+    out: 2D array_like
+        Choi matrix of A, A^R.
+
+    _Author: J.A. de León_
+    '''
+    # FALTA REVISAR ESTOS COMENTARIOS
+    # Calculate number of qubits n and dim(total Hilbert space)
+    n = int(log(E.shape[1], 4))
+    dimHilbert = 2**n
+
+    # Create a 2D square array to storage Choi matrix
+    choiMatrix = np.zeros((4**n,4**n), dtype=complex)
+
+    # Compute the reshuffling of A
+    k = 0 # qbit number label (?)
+    p = 0 # index of the rows after every row in A has been reshaped in a square matrix of size 2**n
+    for i in range(4**n): # Go over each of the 4**n rows of D
+        if i % dimHilbert == 0 and i != 0: # If it has completed a dimHilbert number of rows of A
+          k = k + 1
+          p = 0
+        for j in range(dimHilbert): # Each iteration goes over 4**n elements in a row of D
+          choiMatrix[i][dimHilbert*j:dimHilbert*(j+1)] = E[dimHilbert*k+j].reshape((dimHilbert,dimHilbert))[p]
+        p = p + 1
+
+    return choiMatrix
+
+###############################################################################
+#                EXCLUSIVELY QUANTUM CHANNELS RELATED FUNCTIONS
+
+#      1. PauliChannel: PauliChannel(diagonal) computes the superoperator
+#         of a Pauli quantum channel in computational basis, given its
+#         diagonal elements oh the superoperator in the tensor product of
+#         Pauli matrices basis.
+#      2. CPTest: CPTest(E) tests the complete positivity of a matrix E.
+###############################################################################
+
+
+def PauliChannel(diagonal):
+    '''
+    PauliChannel(diagonal) computes the superoperator of a Pauli quantum channel
+    in computational basis, given its diagonal elements oh the superoperator
+    in the tensor product of Pauli matrices basis.
+
+    ### __Parameters:__
+    diagonal: 1D array_like
+        Diagonal of superoperator in Pauli tensor products.
+
+    ### __Returns:__
+    PauliChannel: 2D array_like
+        Superoperator in computational basis.
+
+    _Author: J.A. de León_
+    '''
+    # Calculate number of qubits n
+    # (Pauli Channel superoperator's dimension is 4**n)
+    n = int(log(len(diagonal), 4))
+
+    # Compute change of basis matrix from Pauli tensor products to computacional basis
+    indices = list(product([0,1,2,3], repeat=n))
+    pauliToComputational = []
+    for i in range(len(indices)):
+      pauliToComputational = np.append(pauliToComputational, Pauli(list(indices[i])).reshape(4**n))
+    pauliToComputational = np.transpose(pauliToComputational.reshape((4**n,4**n)))
+
+    # Compute Pauli channel in computacional basis by making a change of basis
+    # (Pauli channels are diagonal in Pauli tensor products basis)
+    PauliChannel = np.matmul(pauliToComputational, np.matmul(np.diagflat(diagonal), np.linalg.inv(pauliToComputational)))
+
+    return PauliChannel
+
+def CPTest(E):
+    '''
+    CPTest(E) tests the complete positivity of a matrix E.
+
+    ### __Parameters__:
+    E: 2D array_like
+        Superoperator acting on density matrices.
+    ### __Returns__:
+    out: boolean
+        True if CP holds, false if not.
+
+    _Author: J.A. de León_
+    '''
+    # Get the smallest eigenvalue of Choi matrix E^R
+    smallestEigval = min(Chop(np.linalg.eigvals(Reshuffle(E))))
+
+    # Return true if smallest eigenvalue is greater or equal to zero, false otherwise
+    if smallestEigval >= 0:
+        return True
+    else:
+        return False
+
+###############################################################################
+#                TECHNICAL PYTHON PROGRAMMING RELATED FUNCTIONS
+###############################################################################
+
+def MatrixForm(A, limiter='p'):
     """
-    Pretty print of a matrix with different delimiters depending on 'p', 'b'or 'v'
+    MatrixForm(A) prints A in matrix form.
+
+    ### __Parameters:__
+    A: 2D square array_like
+        Matrix.
+    ### __Returns:__
+    out: string
+        A printed in matrix form.
+
+    ### __Example:__
+    MatrixForm(np.identity(2)) = ⎛ 1.0 0.0 ⎞
+                                 ⎝ 0.0 1.0 ⎠
+
+    _Author: A. Cabrera_
     """
     if limiter == 'p':
         ls, li = '⎛', '⎝'
@@ -236,38 +317,42 @@ def matrix_form(matrix, limiter='p'):
         ls, li = '⎡', '⎣'
         rs, ri = ' ⎤', ' ⎦'
     max = 0
-    for row in matrix:
+    for row in A:
         for ele in row:
             if len(str(ele)) > max:
                 max = len(str(ele))
     print_str = ''
-    for i in range(len(matrix)):
+    for i in range(len(A)):
         if i == 0:
             print_str += f'{ls}'
-        elif i==(len(matrix)-1):
+        elif i==(len(A)-1):
             print_str += f'{li}'
         else:
             print_str += '⎢'
-        for j in range(len(matrix[i])):
-            print_str += ' {val:^{max}}'.format(val=matrix[i][j], max=max)
+        for j in range(len(A[i])):
+            print_str += ' {val:^{max}}'.format(val=A[i][j], max=max)
         if i == 0:
             print_str += f'{rs}\n'
-        elif i==(len(matrix)-1):
+        elif i==(len(A)-1):
             print_str += f'{ri}'
         else:
             print_str += ' ⎥\n'
     print(print_str, end='\n\n')
     return
 
-def HSInnerP(A, B):
-	'''
-    Computes the Hilbert-Schimidt inner product.
-
-    Input:
-    - A, B: matrices of same dimension
-
-    Returns Hilbert-Schimidt inner product of A and B.
+# When diagonalizing with Python one can encounter eigenvalues of the order
+# 10^{-15} that are zeros. Chop() appproximates those eigenvalues to 0.
+def Chop(expr, *, max=1e-10):
     '''
+    Chop() appproximates numbers of order _max_ to 0.
 
-	output = np.trace(np.matmul(np.transpose(np.conjugate(A)),B))
-	return output
+    ### __Parameters__:
+    expr: float, array_like
+        Number or array of numbers to chop.
+    ### __Returns__:
+    out: float, array_like
+        Number or array of numbers chopped.
+
+    _Author: J.A. de León_
+    '''
+    return [i if abs(i) > max else 0 for i in expr]
